49343ce15390489a75053a488a3162a6d261319a
Jupegarnica <juan@geekshubs.com>
2020-07-19 13:50:14 +0200

    format

--- src/use.test.js
+++ src/use.test.js
@@ -2,38 +2,48 @@ import check from './index';
 
 describe('check strings', () => {
   test('check with constructor', () => {
+
     expect(() => {
       check(String)('a string');
     }).not.toThrow();
+
     expect(() => {
       check(String)('');
     }).not.toThrow();
+
     expect(() => {
       check(String)("");
     }).not.toThrow();
+
     expect(() => {
       check(String)(``);
     }).not.toThrow();
+
     expect(() => {
       check(Number)('a string');
     }).toThrow();
   });
   test('check with Regex', () => {
+
     expect(() => {
       check(/string/)('a string');
     }).not.toThrow();
+
     expect(() => {
       check(/(string)$/)('a string');
     }).not.toThrow();
+
     expect(() => {
       check(/^(string)/)('a string');
     }).toThrow();
 
   });
   test('check with custom validator', () => {
+
     expect(() => {
       check(val => val.length === 8)('a string');
     }).not.toThrow();
+
     expect(() => {
       check(val => val.length === 5)('a string');
     }).toThrow();
@@ -44,17 +54,21 @@ describe('check strings', () => {
 
 describe('check numbers', () => {
   test('check with constructor', () => {
+
     expect(() => {
       check(String)(33);
     }).toThrow();
+
     expect(() => {
       check(Number)(33);
     }).not.toThrow();
   });
   test('check with custom validator', () => {
+
     expect(() => {
       check(val => val > 0)(33);
     }).not.toThrow();
+
     expect(() => {
       check(val => val < 0)(33);
     }).toThrow();
@@ -63,15 +77,19 @@ describe('check numbers', () => {
 
 describe('check falsy', () => {
   test('check', () => {
+
     expect(() => {
       check(null)(null);
     }).not.toThrow();
+
     expect(() => {
       check(null)('null');
     }).toThrow();
+
     expect(() => {
       check(undefined)(undefined);
     }).not.toThrow();
+
     expect(() => {
       check(undefined)('undefined');
     }).toThrow();
