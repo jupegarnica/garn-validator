66f201c2697854a1c1607c5de1768191d81a140b
Jupegarnica <juan@geekshubs.com>
2020-07-19 11:04:51 +0200

    test fixed

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
 
-  const presets = ['@babel/preset-env', '@babel/preset-react'];
+  const presets = [[
+    '@babel/preset-env',
+    {
+      targets: {
+        node: 'current',
+      },
+    },
+  ], '@babel/preset-react'];
   const plugins = ['@babel/plugin-proposal-class-properties'];
 
   return {
