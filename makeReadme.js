5549e54f06f01a790cc7ec1d35b4f2fdb06cc5c7
juan <jupegarnica@gmail.com>
2020-07-22 17:31:08 +0200

    readme generation improved

--- makeReadme.js
+++ makeReadme.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 
-let readme = fs.readFileSync('./README.raw.md','UTF-8');
-let test = fs.readFileSync('src/use.test.js', 'UTF-8');
+let readme = fs.readFileSync('./README.md','UTF-8');
+let test = fs.readFileSync('tests/use.test.js', 'UTF-8');
 
-readme = readme.replace('// import(use.test.js)', test);
+// console.log(readme.match(/(All\sit\scan\sdo(.|\s)*```js)/))
+readme = readme.replace(/(All\sit\scan\sdo[\s]*```js)(.|\s)*(```)/m, '$1\n' + test +'$3');
 
 
 fs.writeFileSync('./README.md', readme);
