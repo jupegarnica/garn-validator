0e90d1f5ba737a78b311cb5557369e09dc75d260
juan <jupegarnica@gmail.com>
2020-07-23 10:41:52 +0200

    examples tests created to be injected in the readme

--- makeReadme.js
+++ makeReadme.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 
 
 let readme = fs.readFileSync('./README.md','UTF-8');
-let test = fs.readFileSync('tests/use.test.js', 'UTF-8');
+let test = fs.readFileSync('tests/examples.test.js', 'UTF-8');
 
-// console.log(readme.match(/(All\sit\scan\sdo(.|\s)*```js)/))
 readme = readme.replace(/(All\sit\scan\sdo[\s]*```js)(.|\s)*(```)/m, '$1\n' + test +'$3');
 
 
