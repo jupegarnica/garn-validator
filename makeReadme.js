5a388c897e69bcb0ab3506a9970f4629657f12d8
juan <jupegarnica@gmail.com>
2020-07-29 16:34:46 +0200

    make readme add tests shema, validator errors

--- makeReadme.js
+++ makeReadme.js
@@ -2,9 +2,22 @@ const fs = require('fs');
 
 
 let readme = fs.readFileSync('./README.md','UTF-8');
-let test = fs.readFileSync('tests/examples.test.js', 'UTF-8');
 
-readme = readme.replace(/(All\sit\scan\sdo[\s]*```js)(.|\s)*(```)/m, '$1\n' + test +'$3');
+const generate = name => `
+#### ${name}
+
+[${name}](https://github.com/jupegarnica/garn-validator/tree/master/tests/${name})
+
+\`\`\`js
+${fs.readFileSync(`tests/${name}`, 'UTF-8')}
+\`\`\`
+`
+
+let test1 = generate('schema.test.js');
+let test2 = generate('custom-validator.test.js');
+let test3 = generate('errors.test.js');
+
+readme = readme.replace(/(<!-- inject tests -->)(.|\s)*/, '$1' + test1 + test2 + test3);
 
 
 fs.writeFileSync('./README.md', readme);
