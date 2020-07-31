e6075d9e6ef6e89bbaf784b4f50ccec6cef7179b
juan <jupegarnica@gmail.com>
2020-07-31 18:09:37 +0200

    Array.prototype.flat polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -85,6 +85,21 @@ if (typeof AggregateError === "undefined") {
 }
 
 // Array.prototype.flatMap polyfill
+if (!Array.prototype.flat) {
+  Array.prototype.flat = function(depth) {
+    var flattend = [];
+    (function flat(array, depth) {
+      for (let el of array) {
+        if (Array.isArray(el) && depth > 0) {
+          flat(el, depth - 1);
+        } else {
+          flattend.push(el);
+        }
+      }
+    })(this, Math.floor(depth) || 1);
+    return flattend;
+  };
+}
 if (!Array.prototype.flatMap) {
   Array.prototype.flatMap = function() {
     return Array.prototype.map.apply(this, arguments).flat(1);
