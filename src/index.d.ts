3538937fdda0cbaffe693024bf064ba78d32325a
juan <jupegarnica@gmail.com>
2020-08-14 19:41:28 +0200

    definions updated

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,14 @@
 export const AsyncFunction: Function;
 export const GeneratorFunction: Function;
 
+// TODO TypeValidationError
 export declare class AggregateError {
   constructor(errors: any[], message: string);
   name: string;
   errors: any[];
   message: string;
   stack: string;
+  raw: Object;
 }
 export declare class EnumValidationError extends AggregateError {
 }
@@ -25,14 +27,14 @@ export function isValidOrLogAll(type: any): (value: any) => boolean;
 
 export function hasErrors(type: any): (value: any) => null | any[];
 
-export function isValidOrThrowAll(
+export function mustBeOrThrowAll(
   type: any,
-): (value: any) => true | never;
+): (value: any) => any | never;
 
-export function isValidOrThrow(
+export function mustBe(
   type: any,
-): (value: any) => true | never;
+): (value: any) => any | never;
 
-export default isValidOrThrow;
+export default mustBe;
 
 // TODO add mustBe().or()
